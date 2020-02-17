fcae427e2cb6517f16cd34a424e63815d735b0f9
Jegadeesh1997 <60925049+Jegadeesh1997@users.noreply.github.com>
2020-02-17 12:07:30 +0530

    Add files via upload

---
+++ mail/Models/csmodel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mail.Models
+{
+    public class csmodel
+    {
+        public int mailid { get; set; } 
+       public string name { get; set; }
+        public string sub { get; set; }
+        public DateTime Date { get; set; }
+        public string mes{ get; set; }
+        
+    }
+}
